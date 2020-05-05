6bd71052e076e5dec8d0f5ead8bd54312356c95b
AspieSoft <aspiesoftweb@gmail.com>
2020-05-05 14:00:40 -0400

    Update

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // In God We Trust
 
 const miniforge = require('./src/index.min');
-module.exports = miniforge;
+
+module.exports = (function(){
+	let exports = miniforge;
+	exports.build = miniforge.build;
+	exports.runFile = miniforge.runFile;
+	exports.rootDir = miniforge.rootDir;
+	return exports;
+})();
